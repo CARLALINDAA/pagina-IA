1c34fec14605634b08ff52fb8674a162ea645b05
CARLALINDAA <112407093+CARLALINDAA@users.noreply.github.com>
2024-10-16 11:13:03 -0300

    16/10
    16/10

--- Script.js
+++ Script.js
@@ -1,14 +1,19 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-Principal");
+const caixaPergunta = document.querySelector(".caixa-Pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-Resultados");
+const textoResultados = document.querySelector(".texto-Resultados");
 
 const perguntas=[
     {
         enunciado:"Carla está na aula de história e sua professora a fez a seguinte pergunta, em que ano o Brsil foi descoberto?",
             alternativas: [
-                "Em 1500",
-                "Em 1505",
+                {
+                    texto: "Em 1500",
+                    afirmacao:"afirmação",
+                }
+               
+                
             ]
 
     },
@@ -16,8 +21,12 @@ const perguntas=[
     {
         enunciado:"A professora te fez a pergunta que garante 5 pontos na sua média, qual o primeiro governante do Brasil?",
             alternativas: [
-                "Tomé de Souza.",
-                "Duarte da Costa.",
+                {
+                    texto:  "Tomé de Souza.",
+                    afirmacao:"afirmação",
+                }
+              
+             
 
             ]
     },
@@ -25,17 +34,50 @@ const perguntas=[
 
         enunciado:"Muito bem você é bom nisso, saberia responder também qual a duração da guerra dos farrapos? ",
              alternativas: [
-                "De 1835 a 1845.",
-                "De 1845 a 1851.",
-
+                {
+                    texto: "De 1835 a 1845.",
+                    afirmacao:"afirmação",
+                }
+               
+               
         ]
 
     },
     {
         enunciado:"E por fim a pergunta do milhão, qual presidente do brasil morreu antes de assumir seu mandato?.",
             alternativas: [
-                "Tancredo De Almeida Neves.",
-                "Joaquim José Da Silva Xavier (Tiradentes).",
+                {
+                    texto:"Tancredo De Almeida Neves.",
+                    afirmacao:"afirmação",
+                }
+            
+               
             ]
     }
-]
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+
+}
+
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",function(){
+            atual++
+            mostraPergunta();
+    });
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+
+}
+
+mostraPergunta();
+
