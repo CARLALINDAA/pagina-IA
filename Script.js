37044026a656470d8a271e3f9678f7b4936354ee
CARLALINDAA <112407093+CARLALINDAA@users.noreply.github.com>
2024-10-23 11:05:06 -0300

    23/10
    23/10

--- Script.js
+++ Script.js
@@ -1,83 +1,123 @@
-const caixaPrincipal = document.querySelector(".caixa-Principal");
-const caixaPergunta = document.querySelector(".caixa-Pergunta");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultados = document.querySelector(".caixa-Resultados");
-const textoResultados = document.querySelector(".texto-Resultados");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultados = document.querySelector(".texto-resultados");
 
-const perguntas=[
+const perguntas = [
     {
-        enunciado:"Carla está na aula de história e sua professora a fez a seguinte pergunta, em que ano o Brsil foi descoberto?",
-            alternativas: [
-                {
-                    texto: "Em 1500",
-                    afirmacao:"afirmação",
-                }
-               
+        enunciado: "Em que ano ocorreu a 1 guerra mundial",
+        alternativas: [
+            {
+                texto:"1914!",
+                afirmacao:"afirmação"
+            },
+            {
+                texto:"1920!",
+                afirmacao:"afirmação."
                 
-            ]
 
-    },
-
-    {
-        enunciado:"A professora te fez a pergunta que garante 5 pontos na sua média, qual o primeiro governante do Brasil?",
-            alternativas: [
-                {
-                    texto:  "Tomé de Souza.",
-                    afirmacao:"afirmação",
-                }
-              
-             
-
-            ]
-    },
-    {
-
-        enunciado:"Muito bem você é bom nisso, saberia responder também qual a duração da guerra dos farrapos? ",
-             alternativas: [
-                {
-                    texto: "De 1835 a 1845.",
-                    afirmacao:"afirmação",
-                }
-               
-               
+            }
+        
         ]
 
     },
     {
-        enunciado:"E por fim a pergunta do milhão, qual presidente do brasil morreu antes de assumir seu mandato?.",
-            alternativas: [
-                {
-                    texto:"Tancredo De Almeida Neves.",
-                    afirmacao:"afirmação",
-                }
-            
-               
-            ]
-    }
+        enunciado: "Quem foi o primeiro presidente do Brasil?",
+        alternativas: [
+            {
+                texto:"Marechal Deodoro Da Fonseca.",
+                afirmacao:"afirmação"
+            },
+            {
+                texto:"Fernando Collor De Melo.",
+                afirmacao:"afirmação."
+                
+
+            }
+        
+        ]
+
+    },
+    {
+        enunciado: "Qual foi o presidente do Brasil que morreu antes de assumir o seu mandato ?.",
+        alternativas: [
+            {
+                texto:"Tancredo De Almeida Neves.",
+                afirmacao:"afirmação"
+            },
+            {
+                texto:"Joaquim José Da Silva Xavier (Tiradentes).",
+                afirmacao:"afirmação."
+                
+
+            }
+        
+    
+        ]
+
+
+    },
+    {
+        enunciado: "Quantas republicas teve o Brasil?",
+        alternativas: [
+            {
+                texto:"cinco.",
+                afirmacao:"afirmação"
+            },
+            {
+                texto:"sete.",
+                afirmacao:"afirmação"
+                
+
+            }
+        
+        
+        ]
+
+    },
+    {
+        enunciado: "O Brasil em seus dentre os vários marcos da sua história tem um dos mais importantes que é sobre a presidência deste país, as perguntas trazem curiosidades e descobertas sobre essa história a você leitor .",
+        
+
+    },
+
 ];
 
 let atual = 0;
 let perguntaAtual;
+let hitoriaFinal = "";
 
 function mostraPergunta() {
+    if(atual >= perguntas.length){
+        motraReultados();
+        return;
+    }[]
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
-
 }
 
-function mostraAlternativas() {
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",function(){
-            atual++
-            mostraPergunta();
-    });
+        botaoAlternativas.addEventListener("click",() => repostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
-
 }
 
-mostraPergunta();
+function repostaSelecionada(opcaoSelecionada){
+    atual++;
+    mostraPergunta();
+    const afirmacoes = opcaoSelecionada.afirmacoes
+}
 
+function mostraResultados(){
+    caixaPerguntas.textContent = "em 2019...";
+    textoResultados.textContent = hitoriaFinal += afirmacoes + "";
+    caixaAlternativas.textContent = "";
+
+}
+mostraPergunta();
